20c4a62c38250bc7a31021f972967f5b6e95c973
Oliver Westenra <oliver.westenra@ANGRYWAKESORT.workdayinternal.com>
2018-12-10 21:19:08 +1300

    Added loop but it never ends. Figure out the stopping point you silly prick

--- adventCode-2.py
+++ adventCode-2.py
@@ -1,34 +1,37 @@
-file = open("AdventCode-1-input")
 
-# Initialises the variables
-freq = 0          # Frequency of the device
-iterations = 0    # Number of loops through the input
+
+freq = 0
 list = []
 
-lines = file.readlines()
-
-
-# Loops through the lines to add or subtract them from the frquency value
-for i in range(0, len(lines)):
+def freqLoop(freq,list = []):
+	# Initialises the variables
+	freq = freq
+	list = list
+	iterations = 0    # Number of loops through the input	
 	
-	line = lines[i]
-	symbol = line[0]
-	num = line[1:]	
+	file = open("AdventCode-1-input")
+	lines = file.readlines()
+
+
+	# Loops through the lines to add or subtract them from the frquency value
+	for i in range(0, len(lines)):
+		
+		line = lines[i]
+		symbol = line[0]
+		num = line[1:]	
 	
-	if symbol == "-":
-		freq -= int(num)
-	else:
-		freq += int(num)
+		if symbol == "-":
+			freq -= int(num)
+		else:
+			freq += int(num)
 
-	if freq in list:
-		print freq
-		break
-	else:
-		list.append(freq)
-
-
-
-
-print freq
-
+		if freq in list:
+			print freq
+			break
+		else:
+			list.append(freq)
 	
+	freqLoop(freq,list)
+
+
+freqLoop(freq,list)	
