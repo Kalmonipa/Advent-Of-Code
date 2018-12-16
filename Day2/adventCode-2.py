a887df9e2dacafd45c73a539f10cb10e50ed9f31
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2018-12-16 15:13:01 +1300

    Added more code to d2

--- Day2/adventCode-2.py
+++ Day2/adventCode-2.py
@@ -1,22 +1,39 @@
 twoSame = 0
 threeSame = 0
 
-def checkLines(line):
+
+def checkLines(line, twoSame, threeSame):
     chars = []
     counter = []
 
     for char in line:
-        chars.append(char)
-        counter.append(0)
+        counter.append(line.count(char))
 
-        
+    print line
+    print counter
+
+    if 2 in counter:
+        twoSame += 1
+    if 3 in counter:
+        threeSame += 1
+
+    print twoSame
+    print threeSame
+
+
 
 
 def main():
+
 
     file = open("testInput")
 
     lines = file.readlines()
 
     for line in lines:
-        checkLines(line)
+        checkLines(line, twoSame, threeSame)
+
+    print twoSame
+    print threeSame
+
+main()
