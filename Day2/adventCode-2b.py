b7d52b4aa652e14c012e2f3bd7334b7d45b62807
Oliver Westenra <oliver.westenra@ANGRYWAKESORT.workdayinternal.com>
2018-12-28 21:36:32 +1300

    A bit more of 2b

--- Day2/adventCode-2b.py
+++ Day2/adventCode-2b.py
@@ -12,21 +12,25 @@ def main():
     secElement = 1
     char = 0
     loopCounter = 0
+    diffChar = 0 # Counts the number of different characters in the strings. If more than one then it moves on to the next string
 
-    while secElement < len(lines):
-   	if lines[firstElement][char] == lines[secElement][char]:
-            print "Char " + lines[firstElement][char] + " matches in element " + lines[firstElement] + " and " + lines[secElement+1]
-	    if char == len(lines[firstElement]):
-	    	break
-	    else:
-	    	char += 1
-	    loopCounter += 1 
-	else:
-	    firstElement += 1
-	    secElement += 1
-	    char = 0
-	    loopCounter += 1
-    
+    while firstElement < len(lines) and secElement < len(lines):
+        #print "firstEle: " + str(firstElement)
+        #print "secEle: " + str(secElement)
+        if lines[firstElement][char] == lines[secElement][char] and diffChar <= 1:
+            print "Char " + lines[firstElement][char] + " matches in element " + lines[firstElement] + " and " + lines[secElement]
+            if char < len(lines[firstElement]):
+	    	    char += 1
+	            loopCounter += 1
+        else:
+	        #firstElement += 1
+            diffChar += 1
+            secElement += 1
+            char = 0
+            loopCounter += 1
+        if secElement == len(lines):
+            firstElement += 1
+
     print "loopCounter: " + str(loopCounter)
     print lines
 
