562daf433cd0d4675a0fd1f7c9f620f6c605ac0b
Oliver Westenra <oliver.westenra@ANGRYWAKESORT.workdayinternal.com>
2018-12-28 21:06:59 +1300

    Got working on 2b

--- Day2/adventCode-2b.py
+++ Day2/adventCode-2b.py
@@ -2,18 +2,33 @@
 def main():
     file = open("testInput")
 
-    lines = file.readlines()
+    lines = file.read().splitlines()
 
     firstChar = ""
     secondChar = ""
     firstLine = []
     secLine = []
+    firstElement = 0
+    secElement = 1
+    char = 0
+    loopCounter = 0
 
-    for line in lines:
-
-        for char in line:
-            firstLine.append(char)
-
+    while secElement < len(lines):
+   	if lines[firstElement][char] == lines[secElement][char]:
+            print "Char " + lines[firstElement][char] + " matches in element " + lines[firstElement] + " and " + lines[secElement+1]
+	    if char == len(lines[firstElement]):
+	    	break
+	    else:
+	    	char += 1
+	    loopCounter += 1 
+	else:
+	    firstElement += 1
+	    secElement += 1
+	    char = 0
+	    loopCounter += 1
+    
+    print "loopCounter: " + str(loopCounter)
+    print lines
 
 
 main()
