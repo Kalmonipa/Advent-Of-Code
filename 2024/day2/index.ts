66f0ef2cbe613dd87c540f70332450033e26dc12
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-02-22 00:01:21 +0100

    day2

--- 2024/day2/index.ts
+++ 2024/day2/index.ts
@@ -1,4 +1,59 @@
+import * as fs from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+function readFile(filename: string): string {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = join(__filename, '..');
+
+    const content = fs.readFileSync(join(__dirname, filename), 'utf8')
+
+    // console.log("== Content")
+    // console.log(content)
+
+    return content;
+}
+
+function convertToNumber(strArray: string[]): number[] {
+    let numArray: number[] = [];
+
+    strArray.forEach((str) => {
+        numArray.push(Number(str))
+    })
+
+    return numArray;
+}
+
+function getNumArrays(fileName: string) {
+
+
+    const content = readFile(fileName)
+
+    const lines = content.split('\n');
+
+    lines.forEach((line) => {
+        let nums: string[] = line.split("   ");
+        firstArray.push(nums[0])
+        secArray.push(nums[1])
+    })
+
+    let firstNumArray: number[] = convertToNumber(firstArray).sort();
+    let secNumArray: number[] = convertToNumber(secArray).sort();
+
+    return [firstNumArray, secNumArray]
+}
+
 function solvePartOne(fileName: string) {
+    // Both of the following must be true
+    //   - Levels are all increasing or all decreasing
+    //   - Any two adjacent levels differ by at least one and 
+    //     at most three
+
+    let totalSafeLevels: number = 0;
+
+    const content: string = readFile(fileName);
+
+
 
 }
 
