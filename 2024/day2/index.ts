12195eb23207c7e9c3bccc5aad5d1aadb23aa837
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-02-21 23:53:31 +0100

    day2

---
+++ 2024/day2/index.ts
@@ -0,0 +1,10 @@
+function solvePartOne(fileName: string) {
+
+}
+
+export default function solve(mode?: string) {
+    const fileName = mode === "test" ? "test-input.txt" : "input.txt";
+
+    solvePartOne(fileName)
+
+}
