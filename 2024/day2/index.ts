b66189c39ae9104dfab80235c4be49b343046ba6
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-02-22 12:05:43 +0100

    day2 part1

--- 2024/day2/index.ts
+++ 2024/day2/index.ts
@@ -24,23 +24,23 @@ function convertToNumber(strArray: string[]): number[] {
     return numArray;
 }
 
-function getNumArrays(fileName: string) {
-
-
+function getNumArray(fileName: string): number[][] {
+    let numArray: number[][] = [];
     const content = readFile(fileName)
 
     const lines = content.split('\n');
 
+    console.log(lines)
+
     lines.forEach((line) => {
-        let nums: string[] = line.split("   ");
-        firstArray.push(nums[0])
-        secArray.push(nums[1])
+        let reports: string[] = line.split(" ");
+
+        numArray.push(convertToNumber(reports));
     })
 
-    let firstNumArray: number[] = convertToNumber(firstArray).sort();
-    let secNumArray: number[] = convertToNumber(secArray).sort();
+    console.log(numArray)
 
-    return [firstNumArray, secNumArray]
+    return numArray
 }
 
 function solvePartOne(fileName: string) {
@@ -51,9 +51,66 @@ function solvePartOne(fileName: string) {
 
     let totalSafeLevels: number = 0;
 
-    const content: string = readFile(fileName);
+    const content: number[][] = getNumArray(fileName);
 
+    console.log(content)
 
+    content.forEach((level) => {
+        let isSafe = false;
+        let previousState: number = 0;
+        let currentState: number = 0; // If this is 1 the level is increasing, if -1 level is decreasing
+
+        // Set the initial state increasing or decreasing
+        if (level[0] < level[1]) {
+            currentState = 1
+            previousState = currentState
+        } else if (level[0] > level[1]) {
+            currentState = -1
+            previousState = currentState
+        }
+        console.log("Initial state: ", currentState)
+
+        for (let i = 0; i < level.length - 1; i++) {
+            let numOne: number = level[i]
+            let numTwo: number = level[i+1]
+            let comparison: number = numOne - numTwo
+            
+
+            if (numOne < numTwo) {
+                currentState = 1
+            } else if (numOne > numTwo) {
+                currentState = -1
+            }
+            
+            if (currentState !== previousState) {
+                console.log("State is changing. Was", previousState, "is now", currentState);
+                isSafe = false;
+                break;
+            }
+
+            // This checks the second condition
+            if ((3 >= comparison && comparison >= 1) || (-3 <= comparison && comparison <= -1)) {
+                console.log(level[i], "and", level[i+1], "is safe")
+                isSafe = true
+            } else {
+                console.log(level[i], "and", level[i+1], "is unsafe")
+                isSafe = false
+                break
+            }
+            previousState = currentState;
+        }
+        if (isSafe) {
+            console.log("Line is", isSafe);
+            totalSafeLevels += 1;
+        }
+    })
+
+    
+
+    console.log("=======")
+    console.log("Answer to Part 1:")
+    console.log(totalSafeLevels)
+    console.log("=======")
 
 }
 
