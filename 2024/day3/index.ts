d5e0949de0908b6fa45e4f43e359d030a8d9fe7d
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-02-23 19:34:14 +0100

    getting incorrect value for part 2. not sure why

--- 2024/day3/index.ts
+++ 2024/day3/index.ts
@@ -19,30 +19,47 @@ function parseMul(content: string, ind: number): number {
     // Identify if it's a valid mul() phrase
     let mulPhrase: string = content.substring(ind+4, content.indexOf(")", ind));
 
-    logger.debug(mulPhrase)
+    //logger.debug("Phrase found:",mulPhrase)
 
     let strValues: string[] = mulPhrase.split(',');
 
-    logger.debug(strValues)
+    //logger.debug(strValues)
 
-    for (const str of strValues) {
-        if (/^\d+$/.test(str) === false) {
-            logger.debug("mul phrase does not meet requirements")
+    for (let i = 0; i < strValues.length; i++) {
+        if (strValues[i].includes("mul(")) {
+            //logger.debug("Phrase contains another mul phrase", strValues[i])
+            
+            strValues[i] = strValues[i].substring(strValues[i].indexOf("mul(") + 4)
+            //logger.debug("New str is", strValues[i])
+            //logger.debug("New strValues:", strValues)
+            return 0;
+        }
+        if (/^\d+$/.test(strValues[i]) === false) {
+            //logger.debug("Phrase", strValues[i], "does not meet requirements")
             return 0;
         }
     }
 
     let values: number[] = convertToNumber(strValues);
     if (values.length !== 2) {
-        logger.debug("mul phrase does not meet requirements")
+        //logger.debug("mul phrase has too many values")
         return 0;
     }
 
     logger.debug(values)
+    let multipliedValue: number = values[0] * values[1]
+    //logger.debug("Multiplied value is", multipliedValue)
 
-    return values[0] * values[1];
+    return multipliedValue;
 }
 
+function removeDisabledInstructions(content: string, indexOfDisableFlag: number, indexOfEnableFlag: number){
+    //logger.debug("Start of cut:", indexOfDisableFlag)
+    //logger.debug("End of cut:", indexOfEnableFlag)
+    logger.debug("Cut instructions:", content.substring(indexOfDisableFlag, indexOfEnableFlag))
+
+    return content.substring(0,indexOfDisableFlag) + content.substring(indexOfEnableFlag+1);
+  }
 
 function solvePartOne(content: string): number {
     let totalValue: number = 0;
@@ -57,7 +74,6 @@ function solvePartOne(content: string): number {
         ind = ind + 4;
     }
 
-    logger.debug(totalValue.toString())
     return totalValue;
 }
 
@@ -65,18 +81,52 @@ function solvePartTwo(content: string): number {
     // Current thinking is just to cut out the substrings between `don't()` and `do()`
     // and then process the remaining like in part 1
     let totalValue: number = 0;
-    var mulCount = (content.match(/mul\(/g) || []).length
+    var mulCount: number = 0;
+    var disabledFlagsRemoved: number = 0;
     let ind: number = 0
+
+    // Remove don't() and do() sections
+    let indexOfDisableFlag: number = content.indexOf("don't()");
+
+    while (indexOfDisableFlag !== -1) {
+        let indexOfEnableFlag: number = content.indexOf("do()") + 4; // Add 4 to remove the do() as well
+
+        if (indexOfDisableFlag === -1) {
+            logger.debug("Found no more instances of don't()")
+            continue;
+        }
+        while (indexOfEnableFlag < indexOfDisableFlag) {
+            indexOfEnableFlag = content.indexOf('do()', indexOfEnableFlag) + 4;
+            logger.debug("Index of disable flag:", indexOfDisableFlag)
+            logger.debug("Index of enable flag:", indexOfEnableFlag)
+        }
+
+        content = removeDisabledInstructions(content, indexOfDisableFlag, indexOfEnableFlag)
+ 
+        indexOfDisableFlag = content.indexOf("don't()");
+        logger.debug("Index of next disable flag", indexOfDisableFlag)
+
+        disabledFlagsRemoved += 1;
+
+        //logger.debug("New substring:", content)
+    }
+
+    logger.debug("Number of disable flags removed:", disabledFlagsRemoved)
+
+    //logger.debug("New substring:", content)
+
+
+    mulCount = (content.match(/mul\(/g) || []).length
 
     for (let i = 0; i < mulCount; i++) {
         ind = content.indexOf("mul(", ind)
 
-        totalValue += parseMul(content, ind)
+        totalValue = totalValue + parseMul(content, ind)
+        //logger.debug("Total value:", totalValue)
 
         ind = ind + 4;
     }
 
-    logger.debug(totalValue.toString())
     return totalValue;
 }
 
@@ -84,7 +134,7 @@ export default function solve(mode?: string) {
     const fileName = mode === "test" ? "test-input.txt" : "input.txt";
     const content: string = readFile(fileName)
 
-    logger.debug(content)
+    //logger.debug(content)
 
     const partOneAnswer: number = solvePartOne(content)
 
