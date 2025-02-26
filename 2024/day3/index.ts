5620bc27b984b61c5de7ec4a642ecf78c361ec83
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-02-26 21:00:19 +0100

    solved part 2

--- 2024/day3/index.ts
+++ 2024/day3/index.ts
@@ -4,7 +4,13 @@ import * as fs from 'fs';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
 
-const logger = createLogger(LogLevel.INFO); // Change this to DEBUG to view debugging logs
+const logger = createLogger(LogLevel.DEBUG); // Change this to DEBUG to view debugging logs
+
+let mulIdentifier: string = "mul(";
+let disableIdentifier: string = "don't()";
+let enableIdentifier: string = "do()";
+
+let bigLargeNum: number = 100000000;
 
 function readFile(filename: string): string {
     const __filename = fileURLToPath(import.meta.url);
@@ -15,33 +21,43 @@ function readFile(filename: string): string {
     return content;
 }
 
-function parseMul(content: string, ind: number): number {
-    // Identify if it's a valid mul() phrase
-    let mulPhrase: string = content.substring(ind+4, content.indexOf(")", ind));
+// Takes in the string `mul(xxx,xxx`, removes the `mul(` and processes the following characters
+// If they are not valid numbers, returns 0
+// If it is a valid mul phrase, processes it and returns the multiplication and where to start from in the next loop
+function parseMul(content: string, ind: number): {value: number, startingInd: number} {
+    let instrEnd: number = content.indexOf(")", ind)
+    let mulPhrase: string = content.substring(ind, instrEnd);
 
-    logger.debug(mulPhrase)
+    logger.debug("String to parse:", mulPhrase)
+
+    if (mulPhrase.startsWith(mulIdentifier) === false) {
+        logger.debug(mulPhrase,"is not a mul phrase")
+        return {value: 0, startingInd: ind};
+    }
+    mulPhrase = content.substring(ind+4, instrEnd);
+
 
     let strValues: string[] = mulPhrase.split(',');
 
-    logger.debug(strValues)
+    //logger.debug(strValues)
 
     for (const str of strValues) {
         if (/^\d+$/.test(str) === false) {
             logger.debug("mul phrase does not meet requirements")
-            return 0;
+            return {value: 0, startingInd: ind};
         }
     }
 
     let values: number[] = convertToNumber(strValues);
     if (values.length !== 2) {
         logger.debug("mul phrase does not meet requirements")
-        return 0;
+        return {value: 0, startingInd: ind};
     }
 
     const mulValue: number = values[0] * values[1]
     logger.debug("Multiplying", values[0], "by", values[1], "to get", mulValue);
 
-    return mulValue;
+    return {value: mulValue, startingInd: instrEnd};
 }
 
 
@@ -49,57 +65,97 @@ function solvePartOne(content: string): number {
     let totalValue: number = 0;
     var mulCount = (content.match(/mul\(/g) || []).length
     let ind: number = 0
+    let instrEnd: number = 0;
 
     for (let i = 0; i < mulCount; i++) {
         ind = content.indexOf("mul(", ind)
 
-        totalValue += parseMul(content, ind)
+        totalValue += parseMul(content, ind).value
 
         ind = ind + 4;
     }
 
-    logger.debug(totalValue.toString())
     return totalValue;
+}
+
+function findNextIndex(content: string, identifer: string, ind: number): number {
+    let i: number = content.indexOf(identifer, ind) === -1 ? bigLargeNum : content.indexOf(identifer, ind)
+    logger.debug("Setting next", identifer, "index to", i)
+    return i
 }
 
 function solvePartTwo(content: string): number {
     let totalValue: number = 0;
-    var mulCount = (content.match(/mul\(/g) || []).length
-    let ind: number = 0
     let enableInstructions: boolean = true;
-    let mulIdentifier: string = "mul(";
-    let disableIdentifier: string = "don't()";
-    let enableIdentifier: string = "do()";
+    const contentLength: number = content.length;
 
-    for (let i = 0; i < content.length; i++) {
-        let currentChar: string = content[i]
+    // Upcoming mul(), don't() and do()
+    let nextMulIndex: number = content.indexOf(mulIdentifier)
+    let nextDontIndex: number = content.indexOf(disableIdentifier) + 7
+    let nextDoIndex: number = content.indexOf(enableIdentifier);
 
-        // If most recent instruction is do(), look for a don't()
-        if (enableInstructions) {
-            if (content.substring(i, i + 7) === disableIdentifier) {
-                logger.debug("Disabling instructions. Found",content.substring(i, i + 7))
-                enableInstructions = false;
-                i = i + 7
-            }
-        // If most recent instruction is a don't(), look out for a do()
-        } else if (enableInstructions === false) {
-            if (content.substring(i, i + 4) === enableIdentifier) {
-                logger.debug("Enabling instructions. Found", content.substring(i, i + 4))
-                enableInstructions = true;
-                i = i + 5
-            }
+    // Most recent don't() and do()
+    let prevDontIndex: number = -20;
+    let prevDoIndex: number = 0; 
+
+
+    for (let i = 0; i < contentLength; i++) {
+        logger.debug("Looping through", contentLength, "times")
+
+        logger.debug("Instructions are enabled:", enableInstructions)
+        logger.debug("Next:")
+        logger.debug("Mul():", nextMulIndex, "; dont():", nextDontIndex, "; do():", nextDoIndex, "; i:", i)
+
+        // We only want to update these values if we have passed a do() or a dont()
+        if (i >= nextDontIndex) {
+            prevDontIndex = i
+            logger.debug("Setting prev dont index", prevDontIndex)
+
+        }
+        if (i >= nextDoIndex) {
+            prevDoIndex = i
+            logger.debug("Setting prev do index", prevDoIndex)
         }
 
-        //ind = content.indexOf(mulIdentifier, i)
+        let distanceFromDont: number = i - prevDontIndex
+        let distanceFromDo: number = i - prevDoIndex
 
-        if (enableInstructions) {
-            totalValue += parseMul(content, i)
-        } else {
-            logger.debug("Instructions disabled. Not processing", content.substring(i, i+10))
+        logger.debug("Distance from do()", distanceFromDo, "dont()", distanceFromDont)
+
+        if (distanceFromDo < distanceFromDont && !enableInstructions) {
+            logger.debug("Enabling instructions")
+            enableInstructions = true;
+            nextDoIndex = findNextIndex(content, enableIdentifier, i)
+        } else if (distanceFromDo > distanceFromDont && enableInstructions) {
+            logger.debug("Disabling instructions")
+            enableInstructions = false;
+            nextDontIndex = findNextIndex(content, disableIdentifier, i)
         }
+
+        if (i === nextMulIndex && enableInstructions) {
+            let parseMulResult: {value: number, startingInd: number} = parseMul(content, i)
+            totalValue += parseMulResult.value
+
+            if (parseMulResult.startingInd === -1) {
+                logger.debug("Somehow got -1. Breaking")
+                break;
+            }
+
+            logger.debug("Total value is now", totalValue)
+            logger.debug("i is", i, ". Jumping ahead to", parseMulResult.startingInd)
+            i = parseMulResult.startingInd;
+            nextMulIndex = findNextIndex(content, mulIdentifier, i)
+            nextDontIndex = findNextIndex(content, disableIdentifier, i)
+            nextDoIndex = findNextIndex(content, enableIdentifier, i)
+
+        } else if (i > nextMulIndex) {
+            nextDontIndex = findNextIndex(content, disableIdentifier, i)
+            nextDoIndex = findNextIndex(content, enableIdentifier, i)
+            nextMulIndex = findNextIndex(content, mulIdentifier, i)
+        }
+
     }
 
-    logger.debug(totalValue.toString())
     return totalValue;
 }
 
@@ -107,13 +163,11 @@ export default function solve(mode?: string) {
     const fileName = mode === "test" ? "test-input.txt" : "input.txt";
     const content: string = readFile(fileName)
 
-    logger.debug(content)
-
     const partOneAnswer: number = solvePartOne(content)
 
     logger.info("=======")
     logger.info("Answer to Part 1")
-    logger.info(partOneAnswer.toString())
+    logger.info("Expected: 159833790, Actual: ", partOneAnswer)
     logger.info("=======")
 
     const partTwoAnswer: number = solvePartTwo(content)
@@ -122,8 +176,5 @@ export default function solve(mode?: string) {
     logger.info("Answer to Part 2")
     logger.info(partTwoAnswer.toString())
     logger.info("=======")
-
-    // Answers tried:
-    // 111087209
 
 }
