d3a7baa013b59b7e7abc07d1213094464f290b23
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-03-01 16:07:30 +0100

    start part 1

--- 2024/day5/part1.ts
+++ 2024/day5/part1.ts
@@ -1,8 +1,24 @@
 import { createLogger, LogLevel } from "../utils/logger";
 
-const logger = createLogger(LogLevel.INFO); // Change this to DEBUG to view debugging logs
+const logger = createLogger(LogLevel.DEBUG); // Change this to DEBUG to view debugging logs
+
+function parseFile(content: string): string[] {
+    let orderRulesAndUpdates: string[] = content.split(" ")
+
+    if (orderRulesAndUpdates.length === 3) {
+        orderRulesAndUpdates.splice(2)
+    }
+
+    return orderRulesAndUpdates
+}
 
 export function solvePartOne(content: string): number {
+    let pageOrderRules: string = ""
+    let updates: string = ""
+
+    const orders: string[] = parseFile(content)
+    pageOrderRules = orders[0]
+    updates = orders[1]
 
     return 0;
 }
