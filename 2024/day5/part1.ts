77797b6c3d05202fbff0dbcf26fd23bb42148b0e
Oliver Westenra <oliver.westenra@hotmail.co.nz>
2025-03-03 17:01:28 +0100

    counts the number of valid updates

--- 2024/day5/part1.ts
+++ 2024/day5/part1.ts
@@ -1,24 +1,125 @@
 import { createLogger, LogLevel } from "../utils/logger";
+import { convertStringToArray, convertToNumber } from "../utils/helpers"
+import { isBigInt64Array } from "util/types";
 
 const logger = createLogger(LogLevel.DEBUG); // Change this to DEBUG to view debugging logs
 
+let ruleMap = new Map<number, number[]>();
+let totalPartOneValidUpdates: number = 0
+
 function parseFile(content: string): string[] {
-    let orderRulesAndUpdates: string[] = content.split(" ")
+    let orderRulesAndUpdates: string[] = content.split("\n\n")
+
+    logger.debug("Content:",orderRulesAndUpdates.length)
 
     if (orderRulesAndUpdates.length === 3) {
         orderRulesAndUpdates.splice(2)
     }
 
+    // logger.debug("Page order rules:\n", orderRulesAndUpdates[0])
+    // logger.debug("Updates:\n", orderRulesAndUpdates[1])
+
     return orderRulesAndUpdates
 }
 
+function buildRuleDictionary(orderRules: number[][]) {
+    for (let rule of orderRules) {
+        let pageA: number = rule[0]
+        let pageB: number = rule[1]
+
+        if (ruleMap.has(pageA)) {
+            logger.debug("Rule map contains", pageA)
+            let pageRules = ruleMap.get(pageA)
+            logger.debug("Current pages in array:", pageRules)
+            pageRules.push(pageB)
+            ruleMap.set(pageA, pageRules)
+            logger.debug("Pages in array after update:", pageRules)
+
+        } else {
+            logger.debug("Rule map does not contain", pageA)
+            ruleMap.set(pageA, [pageB])
+            logger.debug("Added value", pageB, "to key", pageA)
+        }
+
+    }
+    logger.debug("Map:", ruleMap)
+}
+
+function validateUpdate(numOne: number, numTwo: number): boolean {
+    logger.debug("Comparing", numOne, "and", numTwo)
+    if (ruleMap.has(numOne)) {
+        logger.debug("Rule found for", numOne)
+        if (ruleMap.get(numOne).includes(numTwo)) {
+            logger.debug("+ Key", numOne, "must come before", numTwo)
+            return true
+        } else {
+            logger.debug("x Key", numOne, "cannot come before", numTwo)
+            return false
+        }
+    }
+    return false
+}
+
+function checkUpdate(updates: number[]): boolean {
+    let isValidUpdate: boolean = false
+
+    logger.debug("Updates:",updates)
+    for (let updateA = 0; updateA < updates.length - 1; updateA++) {
+        for (let updateB = updateA+1; updateB < updates.length; updateB++) {
+            isValidUpdate = validateUpdate(updates[updateA], updates[updateB])
+            if (!isValidUpdate) {
+                return false
+            }
+            break
+        }
+    }
+    if (isValidUpdate) {
+        logger.debug(updates, "is a valid update")
+        totalPartOneValidUpdates += 1
+        return true
+    }
+    return false
+    
+}
+
+function convertStringArrayToNumArray(update: string, delimiter: string): number[] {
+    let splitUpdate: string[] = update.split(delimiter)
+    return convertToNumber(splitUpdate)
+}
+
 export function solvePartOne(content: string): number {
-    let pageOrderRules: string = ""
-    let updates: string = ""
+    let pageOrderRulesStr: string[] = []
+    let pageOrderRules: number[][] = []
+    let updatesStr: string[] = []
+    let updates: number[][] = []
 
     const orders: string[] = parseFile(content)
-    pageOrderRules = orders[0]
-    updates = orders[1]
+    pageOrderRulesStr = convertStringToArray(orders[0])
+    updatesStr = convertStringToArray(orders[1])
 
-    return 0;
+    //logger.debug("Page order rules:\n", pageOrderRulesStr)
+    //logger.debug("Updates:\n", updates)
+
+    // Convert order rules from string arrays to num arrays
+    for (let orderRule of pageOrderRulesStr) {
+        pageOrderRules.push(convertStringArrayToNumArray(orderRule, "|"))
+    }
+    
+    // Convert updates as well
+    for (let update of updatesStr) {
+        updates.push(convertStringArrayToNumArray(update, ","))
+    }
+
+    buildRuleDictionary(pageOrderRules)
+
+    for (let update of updates) {
+        logger.debug("=== Checking", update)
+        checkUpdate(update)
+    }
+    
+
+
+    
+
+    return totalPartOneValidUpdates;
 }
--- 2024/utils/helpers.ts
+++ 2024/utils/helpers.ts
@@ -1,11 +1,15 @@
+// Converts a string array to a number array
 export function convertToNumber(strArray: string[]): number[] {
     let numArray: number[] = [];
-
-
 
     strArray.forEach((str) => {
         numArray.push(Number(str))
     })
 
     return numArray;
+}
+
+// Converts a string to an array of strings based on new lines
+export function convertStringToArray(strArray: string): string[] {
+    return strArray.split("\n")
 }
